ea64a1fe88895ac9390d42712e9be4b54b30a3ae
Shubham Gupta <githubemail@emaildomain.com>
2020-11-22 20:09:12 +0530

    made an Api call and fetched data into state.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ import Map from './Map';
 
 function App() {
 
-  const [countries, setCountries] = useState([])
-
+const [countries, setCountries] = useState([])
 const [country, setCountry] = useState("worldwide")
-
+const [countryInfo, setCountryInfo] = useState({})
 // making call to disease API for fetching data for each country.abs
 // https://disease.sh/v3/covid-19/countries
 useEffect(() => {
@@ -35,44 +34,58 @@ useEffect(() => {
 }, [])
 const onCountryChange=async (event)=>{
   const countryCode=event.target.value
-  console.log(countryCode)
   setCountry(countryCode)
+
+const url =countryCode=="worldwide"?"https://disease.sh/v3/covid-19/all":
+                "https://disease.sh/v3/covid-19/countries/".concat(countryCode)
+
+  // https://disease.sh/v3/covid-19/countries/[COUNTRY_CODE]
+ 
+ 
+  await fetch(url)
+ .then(response=> response.json())
+ .then(data=>{
+//  Storing all the data for country or World-wide.
+  setCountryInfo(data)
+ })
+
 }
   return (
-  <div className="App">
+  <div className="app">
 
     <div className="app__left">
       <div className="app_header">
-      {/* <iframe width="1024" height="676" src="https://app.developer.here.com/coronavirus/" frameborder="0"></iframe> */}
-       <h1>COVID 19 TRACKER</h1>
-       <FormControl className="app__dropdown">
-         <Select variant="outlined" value={country} onChange={onCountryChange}>
-        {/* loop through all the countries and show them in the dropdown options. */}
-        <MenuItem value="worldwide">Worldwide</MenuItem>
-        {
-          countries.map((country)=> (
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))
-        }
-         </Select>
-       </FormControl>
-       </div>
+        {/* <iframe width="1024" height="676" src="https://app.developer.here.com/coronavirus/" frameborder="0"></iframe> */}
+         <h1>COVID 19 TRACKER</h1>
+         <FormControl className="app__dropdown">
+           <Select variant="outlined" value={country} onChange={onCountryChange}>
+          {/* loop through all the countries and show them in the dropdown options. */}
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          {
+            countries.map((country)=> (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+           </Select>
+         </FormControl>
+      </div>
   
-       <div className="app__status">
+      <div className="app__stats">
         <InfoBox title="Coronavirus Cases" cases={50}  total={200}></InfoBox>
   
         <InfoBox title="Recovered" cases={50} total={50}></InfoBox>
         
         <InfoBox title="Deaths" cases={50} total={60}></InfoBox>
-        </div>
+      </div>
         <Map></Map>
         
-      </div>
+    </div>
 
-     <Card className="app__right">
-        <CardContent> </CardContent>
-        <CardContent> </CardContent>
-     </Card> 
+     <div className="app_right">
+       <Card >
+         <CardContent> This is Card Content for cases.</CardContent>
+       </Card>
+     </div> 
 
   </div>
 
